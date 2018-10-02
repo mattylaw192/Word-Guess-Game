d9b4b8b12c4bf9cde64b58cfdac12054a6ca1f98
Mattylaw192 <mattylaw192@gmail.com>
2018-10-02 18:30:03 -0400

    Updated notes. Added var for random word.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -5,22 +5,40 @@ var aviato = ['a', 'v', 'i', 'a', 't', 'o'];
 var alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
 var wins = 0;
 var unsolved;
-var wins_Number = document.getElementById ("wins_number")  
+var wins_Number = document.getElementById ("wins_number") ;
 
 
+
+//Gets User Input to start game
 document.onkeyup = function(event) {
 
     var userGuess = event.key.toLowerCase();
 
 }
 
+//Generates random word from Words array
+var randomValue = words[Math.floor(Math.random() * words.length)];
 
+var currentWord = randomValue;
 
+//INSERT FOR LOOP FOR DASHES//
+var i;
+for (i = 0; words.length;  )
 
+//
 
+//Work by small pieces. Use console.log or alert to keep testing
 
+//pick a word
 
+//Create code to "press any key to start game" Test
 
-//How do you get the correct number of _ _ _ for a word?//
+//start with a word. Find length of word. //
+
+//If statement to actually start game. This would actually populate the dashes for the letters//
+
+//Think step by step. Pick a word function//
+
+//How do you get the correct number of _ _ _ for a word? - Try "For Loop"//
 
 //How do you replace a dash with a user guess?//
