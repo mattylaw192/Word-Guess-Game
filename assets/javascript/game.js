bb7d41a2eebeeac28f851dd8ab96291c9d724557
Mattylaw192 <mattylaw192@gmail.com>
2018-10-01 22:45:23 -0400

    Created directories. Added minor html and minor javascript

---
+++ assets/javascript/game.js
@@ -0,0 +1,24 @@
+//Variables
+
+var words = ["aviato", "Gilfoyle", "Hooli", "Smart Fridge", "Palapa", "JIAN YANG", "Special Occasion", "Not A Hot Dog", "Rigby", "PIED PIIIIIIPER"];
+var aviato = ['a', 'v', 'i', 'a', 't', 'o'];
+var alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+var wins = 0;
+var unsolved;
+var wins_Number = document.getElementById ("wins_number")  
+
+
+document.onkeyup = function(event) {
+
+    var userGuess = event.key.toLowerCase();
+    
+}
+
+
+
+
+
+
+
+
+
